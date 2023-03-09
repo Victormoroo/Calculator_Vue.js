16924e42d4117920e10edb300f5c614d160ee2c5
Victormoroo <victorhugo0609@outlook.com>
2023-03-09 05:21:34 -0300

    add: starting project

---
+++ src/components/Calculator.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>Hello Vue!</h1>
+    <p>My name is {{nome}}</p>
+    <p>I'm a {{profissao}}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Calculator',
+
+    data() {
+      return {
+        nome: 'Victor',
+        profissao: 'programming student'
+      }
+    },
+
+    methods: {
+
+    }
+  }
+</script>
